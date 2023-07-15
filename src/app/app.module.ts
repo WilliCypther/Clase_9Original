bdadc78da5e7ded4ac06a994ee2b83ff7233159f
William Cubero <101077927+WilliCypther@users.noreply.github.com>
2023-07-15 08:40:12 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { MenuComponent } from './shared/components/menu/menu.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [ AppComponent, HeaderComponent, FooterComponent, MenuComponent],
+  imports: [BrowserModule,AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
